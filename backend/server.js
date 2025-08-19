d7da2a6d46fcedeff2972a564b6fe392dc5c4bc2
IT22269652 <IT22269652@my.sliit.lk>
2025-08-19 08:32:54 +0530

    connect database

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running port${PORT}`);
+});
